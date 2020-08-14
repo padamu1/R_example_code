71339734bd2f36b329a5aeba41a226ecf24f1160
padamu1 <padamu1@naver.com>
2020-08-14 12:24:47 +0900

    add commit

--- ggplot_add_legend.R
+++ ggplot_add_legend.R
@@ -9,6 +9,6 @@ ggplot(subdataframe, aes(x=spec) )+
   geom_line(aes(y=value3,colour="black"))+
   geom_point(aes(y=value4,colour="green"))+
   geom_line(aes(y=value4,colour="green"))+
-  scale_color_discrete(name="value option",labels = c("value1","value2","value3","value4"))+
+  scale_color_discrete(name="value option",labels = c("value1","value2","value3","value4"))+ ##this value sort down to up
   ggtitle("value spec")+
   theme(plot.title=element_text(face="bold",hjust=0.5,size=15,color = "darkblue"),legend.position = "right")
