43a1c3c34027f2c92b38dc216224f1df05af2b22
padamu1 <padamu1@naver.com>
2020-09-09 22:36:29 +0900

    save plot

--- plot_function/save_plot.R
+++ plot_function/save_plot.R
@@ -1,6 +1,12 @@
-### save plot to png
-
 ### save plot
-png("where you want saving file",width=0,heigth=0) ## open file
+
+### save plot to png - you only save own chart
+png("where you want saving file",width=0,heigth=0) ## open file // size unit is pixel
 plot(model) ## you also use ggplot
+dev.off() ## close file
+
+
+### save plot to pdf - you can save lots of chart
+pdf.options(family = "Korea1deb") ## this is option of pdf's font
+pdf("where you want saving file",width=0,heigth=0) ## open file // size unit is inch
 dev.off() ## close file
